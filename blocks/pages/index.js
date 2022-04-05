ef1fc699e69dbfce10e31d4a6aa1cffc405574b4
markfialko <markfialkofialko@gmail.com>
2022-04-05 21:28:10 +0500

    bugfix(scripts): optimized scripts and added input value

--- blocks/pages/index.js
+++ blocks/pages/index.js
@@ -1,7 +1,10 @@
 import {PopupClose} from "../popup/__close/popup__close.js"
 import {PopupForm} from "../popup/__form/popup__form.js"
 import {Popup} from "../popup/popup.js"
-
-PopupClose();
-PopupForm();
+const editButton = document.querySelector(".user-title__pen");
 Popup();
+editButton.addEventListener('click',function(e){
+    PopupClose();
+    PopupForm();
+    e.preventDefault();
+})
--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,7 +1,11 @@
 export function Popup() {
 
     const editButton = document.querySelector(".user-title__pen");
-
+    let popupForm = document.querySelector(".popup__form");
+    let name = document.querySelector(".user-title__name");
+    let description = document.querySelector(".user-description__subtitle");
+    let userNamePopup = popupForm.children[0];  // input имени
+    let userDescriptionPopup = popupForm.children[1];  // input описания
     function activeBody() {
         let body = document.querySelector("body");
         body.classList.toggle("body_active");
@@ -10,6 +14,8 @@ export function Popup() {
     function activePopup(e) {
         let popup = document.querySelector(".popup");
         popup.classList.toggle("popup_active");
+        userNamePopup.value = name.textContent;
+        userDescriptionPopup.value = description.textContent;
         e.preventDefault();
     }
 
