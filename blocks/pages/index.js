603a4abce3d6868a0a70c84dbe55561a0a40a015
markfialko <markfialkofialko@gmail.com>
2022-05-18 22:51:34 +0500

    project(scripts): refactor main script

--- blocks/pages/index.js
+++ blocks/pages/index.js
@@ -1,25 +1,26 @@
-import {PopupClose} from "../popup/__close/popup__close.js"
-import {
-    PopupUserForm,
-    PopupCardForm,
-    isValid,
-    removeActivePopup,
-    removeActiveBody,
-} from "../popup/__form/popup__form.js"
+import { hideInputError} from "./../input-error/input-error.js"
+import {renderElements} from "./../card/card.js"
+import {openPopup} from "./../popup/popup.js"
+import {enableValidation} from "./../validity/validity.js"
+import {addPenButton,addCardButton} from "./../popup/__button/popup__buton.js"
 
-import {getUserPopup, getCardPopup, activeBody, activePopup} from "../popup/popup.js"
-import {CardList, initialCards, Card, renderElements} from "../card/card.js"
+const penButton = document.querySelector(".user-title__pen");
+penButton.addEventListener('click', function (e) {
+    addPenButton(e);
+})
+
+const cardButton = document.querySelector(".content-info__button");
+cardButton.addEventListener('click', function (e) {
+    addCardButton(e);
+})
 
 renderElements();
-
-const editButtons = document.querySelectorAll(".edit__button");
-getUserPopup();
-getCardPopup();
-editButtons.forEach(button => {
-    button.addEventListener('click', function (e) {
-        PopupClose();
-        PopupUserForm();
-        PopupCardForm();
-        e.preventDefault();
-    })
-})
+enableValidation(
+    {
+        formSelector: '.popup__form',
+        inputSelector: '.form__input',
+        submitButtonSelector: '.popup__button',
+        inactiveButtonClass: 'popup__button_disabled',
+        inputErrorClass: 'form__input_type_error'
+    }
+);
