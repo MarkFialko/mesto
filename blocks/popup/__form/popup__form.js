36a2792b1fc622dd1ff015d7b9b0ae3b0557f5b7
markfialko <markfialkofialko@gmail.com>
2022-05-18 22:51:48 +0500

    project(popup): added popup form script

--- blocks/popup/__form/popup__form.js
+++ blocks/popup/__form/popup__form.js
@@ -1,83 +1,39 @@
-import {initialCards, renderElements} from "../../card/card.js"
+import {renderCard} from "./../../card/card.js"
+import {closePopup} from "./../__close/popup__close.js"
 
-export function isValid(element) {
-    if (element.value.length !== 0) {
-        return true;
+export function getPopup(e,formElement) {
+    if (e.target.closest(".popup__user")) {
+        getUser(formElement);
+        closePopup("popup__user");
+    } else {
+        getCard(formElement);
+        closePopup("popup__card");
     }
-    return false;
 }
 
-export function removeActivePopup(popupSelector) {
-    let popup = document.querySelector(`.${popupSelector}`);
-    popup.classList.remove("popup_active");
-}
+export function getUser(formElement) {
+    const inputName = formElement.elements.user.value;
+    const inputDescription = formElement.elements.description.value;
 
-export function removeActiveBody() {
-    let body = document.querySelector("body");
-    body.classList.remove("body_active");
-
-}
-
-export function PopupUserForm() {
-    const saveButton = document.querySelector(".popup__button");
     let name = document.querySelector(".user-title__name");
     let description = document.querySelector(".user-description__subtitle");
-    const popupForm = document.querySelector(".popup__form");
-    let userNamePopup = popupForm.children[0];
-    let userDescriptionPopup = popupForm.children[1];
-
-    saveButton.addEventListener('click', function (e) {
-        if (isValid(userNamePopup)) {
-            name.innerText = userNamePopup.value;
-            userNamePopup.placeholder = userNamePopup.value;
-        }
-        if (isValid(userDescriptionPopup)) {
-            description.innerText = userDescriptionPopup.value;
-            userDescriptionPopup.placeholder = userDescriptionPopup.value;
-        }
-        removeActivePopup("popup__user");
-        removeActiveBody();
-        e.preventDefault();
-    })
+    name.textContent = inputName;
+    description.textContent = inputDescription;
 }
 
-function renderCard(userNamePopup,userDescriptionPopup) {
-    const card = {
-        name: `${userNamePopup.value}`,
-        link: `${userDescriptionPopup.value}`
-    }
-    initialCards.push(card);
-    renderElements();
-    userNamePopup.value = '';
-    userDescriptionPopup.value = '';
-    removeActivePopup("popup__card");
-    removeActiveBody();
+export function getCard(formElement) {
+    const inputTitle = formElement.elements.cardTitle.value;
+    const inputLink = formElement.elements.cardLink.value;
+    renderCard(inputTitle, inputLink);
+    formElement.reset();
 }
 
-const enterKey = 13;
-
-export function PopupCardForm() {
-    const saveButton = document.querySelector(".popup__button_create");
-    let popupForm = document.querySelector(".popup__form_card");
-
-    let userNamePopup = popupForm.children[0];
-    let userDescriptionPopup = popupForm.children[1];
-
-    userNamePopup.addEventListener('keypress', (e) => {
-        if (isValid(userNamePopup) && isValid(userDescriptionPopup) && e.which === enterKey) {
-            renderCard(userNamePopup,userDescriptionPopup);
-        }
-    })
-
-    userDescriptionPopup.addEventListener('keypress', (e) => {
-        if (isValid(userNamePopup) && isValid(userDescriptionPopup) && e.which === enterKey) {
-            renderCard(userNamePopup,userDescriptionPopup);
-        }
-    })
-
-    saveButton.addEventListener('click', function (e) {
-        if (isValid(userNamePopup) && isValid(userDescriptionPopup)) {
-            renderCard(userNamePopup,userDescriptionPopup);
-        }
-    })
+export function fillUserForm() {
+    const name = document.querySelector(".user-title__name").textContent;
+    const description = document.querySelector(".user-description__subtitle").textContent;
+    const userForm = document.forms.userForm;
+    userForm.elements.user.value = name;
+    userForm.elements.user.placeholder = name;
+    userForm.elements.description.value = description;
+    userForm.elements.description.placeholder = description;
 }
