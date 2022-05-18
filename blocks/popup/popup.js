17ae08e7818a988937fef5bcabb13cf8bba7bfa7
markfialko <markfialkofialko@gmail.com>
2022-05-18 22:51:11 +0500

    project(popup): added active popup and close popup

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,47 +1,21 @@
-export function activeBody() {
-    const body = document.querySelector("body");
-    body.classList.toggle("body_active");
+import {closePopup} from "./__close/popup__close.js"
+
+export function checkEscape(e,popupSelector) {
+    if (e.key === "Escape") {
+        closePopup(popupSelector);
+    }
 }
 
-export function activePopup(
-    popupSelector,
-    userNamePopup = null,
-    userDescriptionPopup = null,
-    name = null,
-    description = null
-) {
+export function openPopup(popupSelector) {
+    document.body.classList.add("body_active");
     const popup = document.querySelector(`.${popupSelector}`)
-    popup.classList.toggle("popup_active");
-    if (name !== null) {
-        userNamePopup.value = name.textContent;
-        userDescriptionPopup.value = description.textContent;
-    }
-}
-
-export function getUserPopup() {
-    const editButton = document.querySelector(".user-title__pen");
-    let popupForm = document.querySelector(".popup__form");
-    let name = document.querySelector(".user-title__name");
-    let description = document.querySelector(".user-description__subtitle");
-    let userNamePopup = popupForm.children[0];  // input имени
-    let userDescriptionPopup = popupForm.children[1];  // input описания
-
-    function toggleActive() {
-        activeBody();
-        activePopup("popup__user", userNamePopup, userDescriptionPopup, name, description);
-    }
-
-    editButton.addEventListener('click', toggleActive)
-}
-
-export function getCardPopup() {
-    const editButton = document.querySelector(".content-info__button");
-    let popupForm = document.querySelector(".popup__form");
-
-    function toggleActive() {
-        activeBody();
-        activePopup("popup__card");
-    }
-
-    editButton.addEventListener('click', toggleActive)
+    popup.classList.add("popup_active");
+    popup.addEventListener('click', (e) => {
+        if (!e.target.closest(".popup__inner") || e.target.closest(".popup__close")) {
+            closePopup(popupSelector);
+        }
+    })
+    document.addEventListener('keydown', (e) => {
+        checkEscape(e,popupSelector);
+    })
 }
