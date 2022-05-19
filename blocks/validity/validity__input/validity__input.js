a2ece860f9478c45c46b57d85fe90b707431aa68
markfialko <markfialkofialko@gmail.com>
2022-05-19 19:07:31 +0500

    project(input): fixed validity spaces

--- blocks/validity/validity__input/validity__input.js
+++ blocks/validity/validity__input/validity__input.js
@@ -1,8 +1,14 @@
 import {showInputError, hideInputError} from "./../../input-error/input-error.js"
+
 export function checkInputValidity(formElement, inputElement, showError = true) {
     if (!inputElement.validity.valid) {
         if (showError) {
             showInputError(formElement, inputElement, inputElement.validationMessage);
+        }
+        return false;
+    } else if (inputElement.value[0] === " ") {
+        if (showError) {
+            showInputError(formElement, inputElement, "Поле не может начинаться с пробела.");
         }
         return false;
     } else {
