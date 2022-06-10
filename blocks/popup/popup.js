22c7b98f80139fe03ad56270cc5e42701682b1ae
markfialko <markfialkofialko@gmail.com>
2022-06-10 21:12:57 +0500

    project(popup): remove find selector from close popup

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -2,7 +2,7 @@ function checkEscape(e) {
     if (e.key === "Escape") {
         const popups = document.querySelectorAll(".popup_active");
         const activePopup = Array.from(popups)[0].className.split(" ")[1];
-        closePopup(activePopup);
+        closePopup();
     }
 }
 
@@ -13,17 +13,18 @@ function openPopup(popupSelector) {
     popup.classList.add("popup_active");
     popup.addEventListener('mousedown', (e) => {
         if (!e.target.closest(".popup__inner") || e.target.closest(".popup__close")) {
-            closePopup(popupSelector);
+            closePopup();
         }
     })
 
     document.addEventListener('keydown', checkEscape);
 }
 
-function closePopup(popupSelector) {
+function closePopup() {
     document.body.classList.remove("body_active");
 
-    const popup = document.querySelector(`.${popupSelector}`);
+    const activePopup = document.querySelector(".popup_active").className.split(" ")[1];
+    const popup = document.querySelector(`.${activePopup}`);
     popup.classList.remove("popup_active");
 
     document.removeEventListener('keydown', checkEscape)
